eca6886e4ef178a26922d11076e15463cad947f2
Andrea <siizx@live.it>
2023-11-05 00:47:22 +0100

    spostata la cartella del labo fuori da questa directory.

---
+++ lect11/es_x_24_ott/Program.fs
@@ -0,0 +1,33 @@
+﻿// 1) Definire la funzione generica genSum : (int -> int) -> int -> int tale che genSum f n calcola f 0 + f 1 + ... + f n. 
+
+let square n = n * n;;
+let cube n = n*n*n;;
+
+let rec genSum f n =
+    if n<0 then 0 else f n + genSum f (n-1);;
+
+let squareSum n = 
+    genSum square n;;
+squareSum 3;;
+
+let cubeSum n =
+    genSum cube n;;
+cubeSum 3;;
+
+
+// =====================================================================
+// =====================================================================
+
+// 2) Definire la funzione generica genProd : (int -> int) -> int -> int tale che genProd f n calcola f 0 * f
+
+let rec genProd f n =
+    if n<2 then 1 else f n * genProd f (n-1);;
+
+let rec fact n =
+    if n<2 then 1 else n * fact (n-1);;
+fact 5;;
+
+let rec twoRaisedTo n =
+    if n<1 then 1 else 2 * twoRaisedTo (n-1);;
+twoRaisedTo 3;; 
+
--- lect11/lect11.fsx/Program.fs
+++ lect11/lect11.fsx/Program.fs
@@ -24,18 +24,76 @@ let f2 = getFun 2;;
 
 f2 3;; // 3 è il secondo argomento della funzione getFun, che è una funzione che prende un argomento e lo somma a x.
 
+// =================================================================================
+// =================================================================================
+
 
 // BOOLEANS
 
 1<0 && 0/0>0;; // false. qua non da errorei perche la prima condizione è falsa, quindi non valuta la seconda.
 0/0>0 && 1<0;; // errore: attempt to divide by zero.
 
-// RECURSIVE FUNCTIONS: REC KEYORD + IF THEN ELSE. PAG 16/22
-let sumSquare n =
-    if n<0 then 0 else n*n+sumSquare(n-1);; (*sumSquare not defined on the right-hand side*)
+////////// RECURSIVE FUNCTIONS: REC KEYORD + IF THEN ELSE. PAG 16/22
 
+// example 1
+//let sumSquare n =
+//    if n<0 then 0 else n*n+sumSquare(n-1);; (*sumSquare not defined on the right-hand side*)
+// Siccome la funzione non e' stata definita con let rec, non puo' essere chiamata ricorsivamente.
+
+
+// =================================================================================
+// =================================================================================
+
+
+// example 2
 let rec sumSquare n =
     if n<0 then 0 else n*n+sumSquare(n-1);; (*sumSquare defined on the right-hand side*)
 
+let rec sumCube n =
+    if n<0 then 0 else n*n*n+sumCube(n-1);; (*sumCube defined on the right-hand side*)
 
-// chiedere a copilot perche sumsquare is not defined nel primo esempio
+// queste due funzioni sono molto simili, quindi possiamo definire 
+// una funzione generica 'genSum' che prende come argomento una funzione.
+
+let square x = x * x
+let cube x = x * x * x
+
+
+let rec genSum f n =
+    if n < 0 then 0 else f n + genSum f (n - 1);;
+
+
+let sumSquare = genSum square
+let sumCube = genSum cube
+
+sumSquare 3;; // = 14
+
+
+
+// =================================================================================
+// funzione fattoriale
+let rec pp p =
+    if p<2 then 1 else p*pp(p-1);;
+pp 5;;
+// =================================================================================
+
+
+
+
+// =================================================================================
+// =================================================================================
+
+////// LISTE
+
+let ls = 2::3::4::[];; // lista di interi
+printfn "%A" ls;;
+
+let ls2 = 5::6::7::8::[];;
+printfn "%A" ls2;;
+
+let lsls = (1::[])::(3::4::[])::(45::32::[])::[];; // lista di liste di interi
+printfn "%A" lsls;;
+
+// concateno le liste
+let catls = ls@ls2;;
+printfn "%A" catls;;

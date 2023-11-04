7ece7646b37169dff8125edebc143d8bffe4b498
Andrea <siizx@live.it>
2023-11-05 00:29:47 +0100

    completato lect12 incluso gli esercizi per casa.

---
+++ lect12/es_x_27_ott/Program.fs
@@ -0,0 +1,45 @@
+﻿// 1) Definire la funzione interval : int -> int -> int list tale che interval a b 
+// restituisce la lista ordinata corrispondente all'intervallo dei numeri interi compresi 
+// tra a e b, estremi inclusi.
+
+let rec interval x y =
+    if x > y then []
+    else interval x (y-1) @ [y];;
+interval (-2) 5;;
+
+// =================================================
+
+// 2) Provare a definire per casi le seguenti funzioni su liste, mostrando i pattern che individuano i 
+// vari casi e le espressioni che corrispondono al valore che deve essere restituito.
+
+//    a) La funzione length: 'a list -> int che calcola la lunghezza di una lista.
+let rec lenght a =
+    match a with 
+        | _::t -> 1 + lenght t
+        | [] -> 0;;
+    
+let lst = [4;6;8;2;0];;
+lenght lst;;
+
+//    b) La funzione sum: int list -> int che calcola la somma di tutti gli elementi di una lista.
+
+let rec sum a =
+    match a with
+        | h::t -> h+sum t
+        | [] -> 0;;
+
+let lst = [4;6;8;2;0];;
+sum lst;;
+
+
+// c) La funzione swap: 'a list -> 'a list che restituisce la lista dove i 
+//    primi due elementi sono stati
+
+let swap a = 
+    match a with
+        | [] -> a;
+        | x::y::t -> y::x::t;;
+
+let lst = [4;6;8;2;0];;
+swap lst;;
+
---
+++ lect12/lect12/Program.fs
@@ -0,0 +1,23 @@
+﻿
+
+
+// questi due sono la stessa cosa. list di interi.
+[2;4];;
+[2]@[4];;
+// questo no. lista di liste di interi:
+[2]::[4::[]];;
+
+let lst = [2;4;6;8];;
+let lst2 = [666]@[2;4;6;8];;
+let lst3 = 999::[2;4;6;8];;
+
+printfn "%A" lst;;
+printfn "%A" lst2;;
+printfn "%A" lst3;;
+
+// =====================================================
+
+let lst = [2;4;6;8];;
+
+let hd (h::t) = h;; // return the head of a list
+hd lst;;
